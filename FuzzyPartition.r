581f6e2f2d0644bcff1ae833607fa739bf6b906c
Dominik <01130556@pw.edu.pl>
2020-04-18 14:12:01 +0200

    Fuzzy Sets

--- FuzzyPartition.r
+++ FuzzyPartition.r
@@ -1,28 +1,31 @@
-##a,b - min i max przedzialu, k-ile zbiorow rozmytych chcemy (im wiecej tym dokladniejsze wyniki)
+#Tworzy pojedynczy zbior rozmyty
+#Na podstawie wartosci lewego i prawego wezla
 
-f<-c(mean,sum)
-f<-function(x){x}
-f(5)
-f(1:5)
-FuzzySet<-function(x,left,right)
+FuzzySet<-function(left,right)
 {
-  mid=(right-left)/2+left
-  mid
-  x
-  x[x<=left|x>=right]<-0
-  
-  
-  a<-(1/(mid-left))
-  a
-  x[x>left&x<=mid]<-a*(x[x>left&x<=mid]-left)
-  
-  x[x>mid&x<right]<- -a*(x[x>mid&x<right]-mid)+1
-  x
+  h<-(right-left)/2
+  mid=left+h
+  a<-1/(mid-left)
+  result<-function(x)
+  {
+    x[x<=left|x>=right]<-0
+    x[x>left&x<=mid]<-a*(x[x>left&x<=mid]-left)
+    
+    x[x>mid&x<right]<- -a*(x[x>mid&x<right]-mid)+1
+    return (x)
+  }
 }
-x<-seq(-3,15,by=0.1)
-plot(x)
 
-plot(FuzzySet(x,1,3))
+#Tworzy liste zbiorow rozmytych
+CreateFuzzySets<-function(nodes)
+{
+  h<-nodes[2]-nodes[1]
+  lapply(nodes,FUN = function(x){FuzzySet(x-h,x+h)})
+}
+
+m<-CreateFuzzySets(1:30)
+m[[1]](seq(1,20,by=0.2))
+
 
 
 
