a0aefd4661cb6d1c3fbcf6eb2500c0ede2edd759
Dominik <01130556@pw.edu.pl>
2020-04-21 12:01:55 +0200

    Discrete Method added

---
+++ CalculateBetasDiscrete.R
@@ -0,0 +1,23 @@
+source("ReadData.R")
+source("FuzzyPartition.R")
+source("FuzzyTransform.R")
+
+
+h <- domain[2] - domain[1]
+nodes <- seq(from=domain[1], length.out = length(domain), by=h)
+fuzzy_sets<-CreateFuzzySets(domain)
+fuzzy_sets <- data.frame(matrix(unlist(fuzzy_sets), nrow=length(fuzzy_sets), byrow=T), stringsAsFactors=FALSE)
+years<-1:length(vector)
+
+tab<-cbind(fuzzy_sets, nodes)
+
+resDiscr<-apply(tab,1,FUN=function(x)
+  {
+  sum(x[[1]](years)*vector*(years-x[[2]]))
+})
+resDiscr
+res
+
+
+
+
